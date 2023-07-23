d72c076321e68ccfc959f9ea23b8ebbc61a01c98
TanmayPShinde <tps.shinde@gmail.com>
2023-07-23 22:54:16 +0530

    feat: horizontal rule below each product

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,9 +8,12 @@ const Home = () => {
   return (
     <div className="mt-5 p-3 max-w-lg mx-auto bg-white rounded-md drop-shadow-lg">
       <h1 className="text-2xl font-semibold ">Products</h1>
-      <hr className="h-px mt-2 mb-3 bg-gray-200 border-2 dark:bg-gray-700" />
+      <hr className="h-px mt-2 mb-3 bg-orange-600 border-2 dark:bg-gray-700" />
       {products.map((product) => (
-        <Product product={product} key={product.id} />
+        <>
+          <Product product={product} key={product.id} />
+          <hr className="h-px my-0 bg-gray-200 border-1 dark:bg-gray-700" />
+        </>
       ))}
     </div>
   );
