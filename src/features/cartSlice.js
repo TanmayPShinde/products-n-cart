f84f22b7372409392169ef58d523bea48a3bb5bb
TanmayPShinde <tps.shinde@gmail.com>
2023-07-23 17:18:20 +0530

    feat: cartSlice

---
+++ src/features/cartSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      console.log(action);
+      const item = action.payload;
+      let productItem = state.cartItems.find(
+        (product) => product.id === item.id
+      );
+      if (productItem) {
+        productItem.quantity += 1;
+      } else {
+        state.cartItems = [item, ...state.cartItems];
+      }
+    },
+    incrementQuantity: (state, action) => {
+      const item = action.payload;
+      let productItem = state.cartItems.find(
+        (product) => product.id === item.id
+      );
+      if (productItem) {
+        productItem.quantity += 1;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const item = action.payload;
+      let productItem = state.cartItems.find(
+        (product) => product.id === item.id
+      );
+      if (productItem) {
+        productItem.quantity -= 1;
+        if (productItem.quantity === 0) {
+          state.cartItems = state.cartItems.filter(
+            (product) => product.id !== item.id
+          );
+        }
+      }
+    },
+    removeFromCart: (state, action) => {
+      const item = action.payload;
+      state.cartItems = state.cartItems.filter(
+        (product) => product.id !== item.id
+      );
+    },
+  },
+});
+
+export default cartSlice.reducer;
