74128ec481f4470f698db828c25a6e9249794b8c
TanmayPShinde <tps.shinde@gmail.com>
2023-07-23 23:22:59 +0530

    feat: utility function check_for_offers

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,4 +8,19 @@ export const check_for_soup = (cartItems) => {
   return false;
 };
 
-export const check_for_offers = (itemId, isSoupPresent) => {};
+// returns the savings on an item if offer applicable
+export const check_for_offers = (item, itemPrice, isSoupPresent) => {
+  // buy 1 get 1 free on cheese!
+  // buy a soup and get bread for half the price!
+  // get 1/3rd off on butter!
+  switch (item.id) {
+    case 1:
+      // Bread
+      if (isSoupPresent) return itemPrice / 2;
+      break;
+    case 5:
+      return itemPrice / 3;
+    default:
+      return 0;
+  }
+};
